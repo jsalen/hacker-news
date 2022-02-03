0ae28787b0b1f37c6e663469430b342097109c76
Joseph Salen <jsalen.91@gmail.com>
2022-02-03 16:46:46 -0400

    [fix] change data state type and fix data extraction from api

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { News } from '../interfaces/News.interface'
 
 const URL_BY_DATE =
   'https://hn.algolia.com/api/v1/search_by_date?hitsPerPage=15'
 
 function useFetch(param: string) {
-  const [data, setData] = useState<any>(null)
+  const [data, setData] = useState<News[]>([])
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<boolean>(false)
 
@@ -16,7 +17,7 @@ function useFetch(param: string) {
       axios
         .get(URL_BY_DATE)
         .then((res) => {
-          setData(res.data)
+          setData(res.data.hits)
           setLoading(false)
         })
         .catch((err) => {
