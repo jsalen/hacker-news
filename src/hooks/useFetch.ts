74564a438eeba7a1a0448eb3ae19ab8878da8edc
Joseph Salen <jsalen.91@gmail.com>
2022-02-03 15:11:04 -0400

    [feature] create useFetch hook to retrieve data from API

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const URL_BY_DATE =
+  'https://hn.algolia.com/api/v1/search_by_date?hitsPerPage=15'
+
+function useFetch(param: string) {
+  const [data, setData] = useState<any>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
+
+  useEffect(() => {
+    setLoading(true)
+
+    if (param === '') {
+      axios
+        .get(URL_BY_DATE)
+        .then((res) => {
+          setData(res.data)
+          setLoading(false)
+        })
+        .catch((err) => {
+          setError(true)
+          setLoading(false)
+        })
+    } else {
+      axios
+        .get(
+          `https://hn.algolia.com/api/v1/search_by_date?query=${param}&page=0&hitsPerPage=15`
+        )
+        .then((res) => {
+          const filteredData = res.data.hits.filter(
+            (
+              author: string,
+              story_title: string,
+              story_url: string,
+              created_at: string
+            ) =>
+              author !== 'null' &&
+              story_title !== 'null' &&
+              story_url !== 'null' &&
+              created_at !== 'null'
+          )
+          setData(filteredData)
+          setLoading(false)
+        })
+        .catch((err) => {
+          setError(true)
+          setLoading(false)
+        })
+    }
+  }, [param])
+
+  return { data, loading, error }
+}
+
+export default useFetch
