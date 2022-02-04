83f6add34747863d7cd7128051e961c60673a5ca
Joseph Salen <jsalen.91@gmail.com>
2022-02-04 15:32:51 -0400

    [fix] filter news omitting nullish info

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,11 +2,6 @@ import { News } from '../interfaces/News.interface'
 
 export const filterData = (data: News[]) => {
   return data.filter((hit: News) => {
-    return (
-      hit.story_title !== null ||
-      hit.author !== null ||
-      hit.story_url !== null ||
-      hit.created_at !== null
-    )
+    return hit.story_title && hit.story_url && hit.author && hit.created_at
   })
 }
